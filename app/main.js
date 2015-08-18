36de0f8247878f8d507c465814b0e47da3818a30
Amar Krupalija <amc0kru@gmail.com>
2015-08-18 14:20:46 +0200

    v.0.2
    HelloWorld done

--- app/main.js
+++ app/main.js
@@ -1,14 +1,11 @@
 var app = require('app');
-bar browserWindow = require('browser-window');
+var BrowserWindow = require('browser-window');
 
-//Disable crash-reporter
 //require('crash-reporter').start();
-
 var mainWindow = null;
 
-app.on("window-all-closed", function() {
-
-  if(process.platform != 'darwin'){
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
     app.quit();
   }
 });
@@ -17,9 +14,9 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   mainWindow.loadUrl('file://index.html');
-  mainWindow.openDevTools();
+//  mainWindow.openDevTools();
 
-  mainWindows.on('closed', function(){
-    mainWindow = null;
+  mainWindow.on('closed', function() {
+  mainWindow = null;
   });
 });
