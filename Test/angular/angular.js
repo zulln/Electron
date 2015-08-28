4cc7787da3fb2e75f7427ceebe7c1a9a610a9531
Amar Krupalija <amc0kru@gmail.com>
2015-08-28 14:01:29 +0200

    Hooked up button to ng-click, calling function in controller

--- Test/angular/angular.js
+++ Test/angular/angular.js
@@ -2,4 +2,8 @@ var angularApp = angular.module('angularApp', []);
 
 angularApp.controller('angularAppCtrl', function($scope){
 	$scope.count = 900;
+
+	$scope.go = function() { alert("Alert!"); }
 });
+
+<!-- NodeJS -->
