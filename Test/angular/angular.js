af0ebe4760f7b13ea47da0ce68849658678c4761
Amar Krupalija <amc0kru@gmail.com>
2015-08-28 14:25:47 +0200

    Built C++ addon for Node

--- Test/angular/angular.js
+++ Test/angular/angular.js
@@ -4,6 +4,11 @@ angularApp.controller('angularAppCtrl', function($scope){
 	$scope.count = 900;
 
 	$scope.go = function() { alert("Alert!"); }
+//	$scope.go = nodeTest;
 });
 
 <!-- NodeJS -->
+
+var addon = require('./Node/build/Release/addonMac');
+
+var nodeTest = console.log(addon.hello());
