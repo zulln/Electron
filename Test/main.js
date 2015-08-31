dada9cb947f603fe8e33114bd28fdaf8a5004717
Amar Krupalija <amc0kru@gmail.com>
2015-08-31 20:55:06 +0200

    Re-sync

--- Test/main.js
+++ Test/main.js
@@ -1,6 +1,6 @@
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
-
+//
 // Report crashes to our server.
 require('crash-reporter').start();
 
