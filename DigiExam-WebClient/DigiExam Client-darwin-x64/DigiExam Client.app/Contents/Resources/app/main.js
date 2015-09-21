a1bd2e2f9c4f6ca8e1dec4f1a8849ae0961932d3
Amar Krupalija <amc0kru@gmail.com>
2015-09-21 10:39:01 +0200

    Revert "Fixed delimiter problem so that the application works with saving offline files on both Win32 and Darwin"
    This reverts commit 15aaa3be0ddc8b92142780225e1a279af0916c8f.

--- DigiExam-WebClient/DigiExam Client-darwin-x64/DigiExam Client.app/Contents/Resources/app/main.js
+++ DigiExam-WebClient/DigiExam Client-darwin-x64/DigiExam Client.app/Contents/Resources/app/main.js
@@ -1,24 +1,19 @@
 var app = require('app');
 var browserWindow = require('browser-window');
 var globalShortcut = require('global-shortcut');
-var dialog = require("dialog");
 
 mainWindow = null;
 
 app.on('ready', function(){
-	mainWindow = new browserWindow({width: 1200, height: 1600});
-
-	mainWindow.webContents.on('did-finish-load', function(){
-		mainWindow.webContents.executeJavaScript("window.isElectron = true;");
-	});
+	mainWindow = new browserWindow({width: 600, height: 800});
 
 	mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
 	globalShortcut.register('Super+r', function(){
 		//Register in order to disable cmd button on Mac and Ctrl on Windows
 	})
-	mainWindow.openDevTools();
 
+	mainWindow.openDevTools();
 
 	mainWindow.on('close', function(){
 		mainWindow = null;
