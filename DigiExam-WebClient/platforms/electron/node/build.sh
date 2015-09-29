56188ebf176da0d5606bfd7c87317aa69ccf9436
Amar Krupalija <amc0kru@gmail.com>
2015-09-29 14:10:19 +0200

    Build bash script updated with arch argument

--- DigiExam-WebClient/platforms/electron/node/build.sh
+++ DigiExam-WebClient/platforms/electron/node/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#Build script for node-gyp
+#Input parameter should be the arch (x64/ia32) of the target machine
+
+arch="ia32"
+if [ $arch != "$1" ]; then
+	arch=$1
+fi
+
 url=https://atom.io./download/atom-shell
 version=$(electron -v | cut -c 2-)
-node-gyp rebuild --target=$version --arch=x64 --dist-url=$url
+node-gyp rebuild --target=$version --arch=$arch --dist-url=$url
