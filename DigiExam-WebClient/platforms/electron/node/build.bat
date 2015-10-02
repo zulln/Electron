176e3ff715830aeb445045d5c4fef8c7794bf445
Amar Krupalija <amc0kru@gmail.com>
2015-10-02 13:46:52 +0200

    Updated the windows build script to include automatic Electron version detection

--- DigiExam-WebClient/platforms/electron/node/build.bat
+++ DigiExam-WebClient/platforms/electron/node/build.bat
@@ -1,7 +1,16 @@
 ::Batch script for building native NodeJS modules for Windows (X86)
 @echo off
 set version=%1
-if [%1]==[] set version=0.33.3
+if [%1]==[] (
+  @echo off
+  setlocal EnableDelayedExpansion
+  set c=0
+  for /f "tokens=2 delims=:, " %%a in (' find ":" ^< "%userprofile%\AppData\Roaming\npm\node_modules\electron-prebuilt\package.json" ') do (
+     set /a c+=1
+     set val[!c!]=%%~a
+  )
+  set version=!val[2]!
+)
 ::Currently defaulting to 0.33.3 if no argument is set
 ::TODO: Implement electron --version functionality
 set arch=ia32
